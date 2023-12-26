99b9b2df67e888db0d21a27fbf2bdab806151d5a
Mitja Leino <mitja.leino@hotmail.com>
2023-12-26 17:41:25 +0200

    Move creating listeners to their own function. Create initial function to update the search list

--- src/main/kotlin/com/mituuz/fuzzier/Fuzzier.kt
+++ src/main/kotlin/com/mituuz/fuzzier/Fuzzier.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentIterator
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.ui.popup.JBPopup
@@ -64,54 +65,9 @@ class Fuzzier : AnAction() {
             // Data has been loaded
             component.fileList.setPaintBusy(false)
 
-            // Add listener that updates the contents of the preview pane
-            component.fileList.addListSelectionListener { event ->
-                run {
-                    if (!event.valueIsAdjusting) {
-                        val selectedValue = component.fileList.selectedValue
-                        val file =
-                            VirtualFileManager.getInstance().findFileByUrl("file://$projectBasePath$selectedValue")
-
-                        file?.let {
-                            val document = FileDocumentManager.getInstance().getDocument(it)
-                            component.previewPane.text = document?.text ?: "Cannot read file"
-                        }
-                    }
-                }
+            if (projectBasePath != null) {
+                createListeners(project, projectBasePath)
             }
-
-            // Add MouseListener for double-click
-            component.fileList.addMouseListener(object : MouseAdapter() {
-                override fun mouseClicked(e: MouseEvent) {
-                    if (e.clickCount == 2) {
-                        val selectedValue = component.fileList.selectedValue
-                        val virtualFile = VirtualFileManager.getInstance().findFileByUrl("file://$projectBasePath$selectedValue")
-
-                        // Open the file in the editor
-                        virtualFile?.let {
-                            popup?.cancel()
-                            FileEditorManager.getInstance(project).openFile(it, true)
-                        }
-                    }
-                }
-            })
-
-            // Add listener that opens the currently selected file when pressing enter (focus on the text box)
-            val enterKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0)
-            val enterActionKey = "openFile"
-            val inputMap = component.searchField.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
-            inputMap.put(enterKeyStroke, enterActionKey)
-            component.searchField.actionMap.put(enterActionKey, object : AbstractAction() {
-                override fun actionPerformed(e: ActionEvent?) {
-                    val selectedValue = component.fileList.selectedValue
-                    val virtualFile = VirtualFileManager.getInstance().findFileByUrl("file://$projectBasePath$selectedValue")
-
-                    virtualFile?.let {
-                        popup?.cancel()
-                        FileEditorManager.getInstance(project).openFile(it, true)
-                    }
-                }
-            })
         }
 
         val mainWindow = WindowManager.getInstance().getIdeFrame(p0.project)?.component
@@ -124,5 +80,78 @@ class Fuzzier : AnAction() {
                     .createPopup()
             popup!!.showInCenterOf(it)
         }
+    }
+
+    fun updateListContents(project: Project) {
+        val listModel = DefaultListModel<String>()
+        val projectFileIndex = ProjectFileIndex.getInstance(project)
+        val projectBasePath = project.basePath
+
+        val contentIterator = ContentIterator { file: VirtualFile ->
+            if (!file.isDirectory) {
+                val filePath = projectBasePath?.let { it1 -> file.path.removePrefix(it1) }
+                if (!filePath.isNullOrBlank()) {
+                    listModel.addElement(filePath)
+                }
+            }
+            true
+        }
+
+        projectFileIndex.iterateContent(contentIterator)
+        component.fileList?.model = listModel
+
+        // Data has been loaded
+        component.fileList.setPaintBusy(false)
+    }
+
+    private fun createListeners(project: Project, projectBasePath: String) {
+        // Add listener that updates the contents of the preview pane
+        component.fileList.addListSelectionListener { event ->
+            run {
+                if (!event.valueIsAdjusting) {
+                    val selectedValue = component.fileList.selectedValue
+                    val file =
+                        VirtualFileManager.getInstance().findFileByUrl("file://$projectBasePath$selectedValue")
+
+                    file?.let {
+                        val document = FileDocumentManager.getInstance().getDocument(it)
+                        component.previewPane.text = document?.text ?: "Cannot read file"
+                    }
+                }
+            }
+        }
+
+        // Add MouseListener for double-click
+        component.fileList.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent) {
+                if (e.clickCount == 2) {
+                    val selectedValue = component.fileList.selectedValue
+                    val virtualFile = VirtualFileManager.getInstance().findFileByUrl("file://$projectBasePath$selectedValue")
+
+                    // Open the file in the editor
+                    virtualFile?.let {
+                        popup?.cancel()
+                        FileEditorManager.getInstance(project).openFile(it, true)
+                    }
+                }
+            }
+        })
+
+        // Add listener that opens the currently selected file when pressing enter (focus on the text box)
+        val enterKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0)
+        val enterActionKey = "openFile"
+        val inputMap = component.searchField.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+        inputMap.put(enterKeyStroke, enterActionKey)
+        component.searchField.actionMap.put(enterActionKey, object : AbstractAction() {
+            override fun actionPerformed(e: ActionEvent?) {
+                val selectedValue = component.fileList.selectedValue
+                val virtualFile = VirtualFileManager.getInstance().findFileByUrl("file://$projectBasePath$selectedValue")
+
+                virtualFile?.let {
+                    popup?.cancel()
+                    FileEditorManager.getInstance(project).openFile(it, true)
+                }
+            }
+        })
     }
 }
