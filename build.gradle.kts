15d2e3344dbcaa87ede6bcd6c3e44e6c698e5cb2
Mitja Leino <mitja.leino@hotmail.com>
2024-01-09 19:58:56 +0200

    Remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ repositories {
 }
 
 dependencies {
-  testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
