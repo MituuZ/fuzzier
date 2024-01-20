0f00255affecd2c636e8c7862f4a853e88187d0f
MituuZ <55958056+MituuZ@users.noreply.github.com>
2024-01-20 08:27:52 +0200

    Limit preview size (#14)
    * Works, but null handling fails
    
    * Empty the preview between files and update line correctly
    
    * Load lines in a chunk instead of 1 by 1
    
    * Reduce chunk size and reset vertical scroll position
    
    * Initial version of small/large file handling. Use prev commit for a working one
    
    * A buggy implementation
    
    * Implement loading the preview based on the lenght of the file
    
    * Fix scrolling not resetting correctly
    
    * Fix large files not sliced correctly
    
    * Add additional change notes

--- src/main/kotlin/com/mituuz/fuzzier/PreviewEditor.kt
+++ src/main/kotlin/com/mituuz/fuzzier/PreviewEditor.kt
@@ -1,7 +1,9 @@
 package com.mituuz.fuzzier
 
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.fileEditor.FileDocumentManager
@@ -9,13 +11,17 @@ import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.fileTypes.PlainTextFileType
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.EditorTextField
+import kotlin.math.min
 
 class PreviewEditor(project: Project?) : EditorTextField(
     project,
     getDefaultFileType()
 ) {
+    private val toBeContinued: String = "\n\n\n--- End of Fuzzier Preview ---\n--- Open file to see full content ---\n\n\n"
+    private val fileCutOff: Int = 70000
 
     companion object {
         fun getDefaultFileType(): FileType {
@@ -43,17 +49,34 @@ class PreviewEditor(project: Project?) : EditorTextField(
 
     fun updateFile(virtualFile: VirtualFile?) {
         ApplicationManager.getApplication().executeOnPooledThread {
-            val document = ApplicationManager.getApplication().runReadAction<Document?> {
+            val sourceDocument = ApplicationManager.getApplication().runReadAction<Document?> {
                 virtualFile?.let { FileDocumentManager.getInstance().getDocument(virtualFile) }
             }
             val fileType = virtualFile?.let { FileTypeManager.getInstance().getFileTypeByFile(virtualFile) }
 
             ApplicationManager.getApplication().invokeLater {
-                if (document != null) {
-                    this.document = document
+                if (sourceDocument != null) {
+                    val endIndex = min(sourceDocument.textLength, fileCutOff)
+                    val previewText = sourceDocument.getText(TextRange(0, endIndex))
+                    WriteCommandAction.runWriteCommandAction(project) {
+                        if (endIndex >= fileCutOff) {
+                            // Create a new document which can be modified without changing the original file
+                            // Only gets partial highlighting
+                            this.document = EditorFactory.getInstance().createDocument(previewText)
+                            this.document.insertString(endIndex, toBeContinued)
+                        } else {
+                            // Use the actual document to enable full highlighting in the preview
+                            this.document = sourceDocument
+                        }
+                        ApplicationManager.getApplication().invokeLater {
+                            editor?.scrollingModel?.run {
+                                scrollHorizontally(0)
+                                scrollVertically(0)
+                            }
+                        }
+                    }
                 }
                 this.fileType = fileType
-                this.editor?.scrollingModel?.scrollHorizontally(0)
             }
         }
     }
