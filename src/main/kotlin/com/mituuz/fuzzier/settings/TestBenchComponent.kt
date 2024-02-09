4cd3144ebfeeab51231d88df521af2cd15e20754
Mitja Leino <mitja.leino@hotmail.com>
2024-02-09 17:19:57 +0200

    Add visible score to test bench

--- src/main/kotlin/com/mituuz/fuzzier/settings/TestBenchComponent.kt
+++ src/main/kotlin/com/mituuz/fuzzier/settings/TestBenchComponent.kt
@@ -120,7 +120,7 @@ class TestBenchComponent : JPanel() {
             }
             val sortedList = listModel.elements().toList().sortedByDescending { it.score }
             val valModel = DefaultListModel<String>()
-            sortedList.forEach { valModel.addElement(it.string) }
+            sortedList.forEach { valModel.addElement("${it.string} (${it.score})") }
 
             SwingUtilities.invokeLater {
                 fileList.model = valModel
