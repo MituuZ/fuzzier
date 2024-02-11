4812632793da073531e83464867c763f0ef0a27b
Mitja Leino <mituuuuz@hotmail.com>
2024-02-11 20:52:26 +0200

    Add initial version of file move action

--- src/main/kotlin/com/mituuz/fuzzier/FuzzyMover.kt
+++ src/main/kotlin/com/mituuz/fuzzier/FuzzyMover.kt
@@ -2,8 +2,113 @@ package com.mituuz.fuzzier
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.openapi.components.service
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler
+import com.intellij.openapi.editor.actionSystem.EditorActionManager
+import com.intellij.openapi.ui.popup.JBPopup
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.ui.popup.JBPopupListener
+import com.intellij.openapi.ui.popup.LightweightWindowEvent
+import com.intellij.openapi.util.DimensionService
+import com.intellij.openapi.wm.WindowManager
+import com.mituuz.fuzzier.components.SimpleFinderComponent
+import com.mituuz.fuzzier.settings.FuzzierSettingsService
+import javax.swing.SwingUtilities
 
 class FuzzyMover : AnAction() {
+    private lateinit var component: SimpleFinderComponent
+    private var fuzzierSettingsService = service<FuzzierSettingsService>()
+    private var popup: JBPopup? = null
+    private val dimensionKey: String = "FuzzyMoverPopup"
+    private lateinit var originalDownHandler: EditorActionHandler
+    private lateinit var originalUpHandler: EditorActionHandler
+
     override fun actionPerformed(p0: AnActionEvent) {
+        setCustomHandlers()
+        SwingUtilities.invokeLater {
+            p0.project?.let { project ->
+                component = SimpleFinderComponent(project)
+                val projectBasePath = project.basePath
+//                if (projectBasePath != null) {
+//                    createListeners(project, projectBasePath)
+//                }
+
+                val mainWindow = WindowManager.getInstance().getIdeFrame(p0.project)?.component
+                mainWindow?.let {
+                    popup = JBPopupFactory
+                        .getInstance()
+                        .createComponentPopupBuilder(component, component.searchField)
+                        .setFocusable(true)
+                        .setRequestFocus(true)
+                        .setResizable(true)
+                        .setDimensionServiceKey(project, dimensionKey, true)
+                        .setTitle("Move Files")
+                        .setMovable(true)
+                        .setShowBorder(true)
+                        .createPopup()
+
+                    popup?.addListener(object : JBPopupListener {
+                        override fun onClosed(event: LightweightWindowEvent) {
+                            resetOriginalHandlers()
+                            super.onClosed(event)
+                        }
+                    })
+                    if (fuzzierSettingsService.state.resetWindow) {
+                        DimensionService.getInstance().setSize(dimensionKey, null, project)
+                        DimensionService.getInstance().setLocation(dimensionKey, null, project)
+                        fuzzierSettingsService.state.resetWindow = false
+                    }
+                    popup!!.showInCenterOf(it)
+                }
+            }
+        }
+    }
+
+    fun resetOriginalHandlers() {
+        val actionManager = EditorActionManager.getInstance()
+        actionManager.setActionHandler(IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN, originalDownHandler)
+        actionManager.setActionHandler(IdeActions.ACTION_EDITOR_MOVE_CARET_UP, originalUpHandler)
+    }
+
+    private fun setCustomHandlers() {
+        val actionManager = EditorActionManager.getInstance()
+        originalDownHandler = actionManager.getActionHandler(IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN)
+        originalUpHandler = actionManager.getActionHandler(IdeActions.ACTION_EDITOR_MOVE_CARET_UP)
+
+        actionManager.setActionHandler(IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN, FuzzyListActionHandler(this, false))
+        actionManager.setActionHandler(IdeActions.ACTION_EDITOR_MOVE_CARET_UP, FuzzyListActionHandler(this, true))
+    }
+
+    fun moveListUp() {
+        val selectedIndex = component.fileList.selectedIndex
+        if (selectedIndex > 0) {
+            component.fileList.selectedIndex = selectedIndex - 1
+            component.fileList.ensureIndexIsVisible(selectedIndex - 1)
+        }
+    }
+
+    fun moveListDown() {
+        val selectedIndex = component.fileList.selectedIndex
+        val length = component.fileList.model.size
+        if (selectedIndex < length - 1) {
+            component.fileList.selectedIndex = selectedIndex + 1
+            component.fileList.ensureIndexIsVisible(selectedIndex + 1)
+        }
+    }
+
+    class FuzzyListActionHandler(private val fuzzyMover: FuzzyMover, private val isUp: Boolean) : EditorActionHandler() {
+        override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext?) {
+            if (isUp) {
+                fuzzyMover.moveListUp()
+            } else {
+                fuzzyMover.moveListDown()
+            }
+
+            super.doExecute(editor, caret, dataContext)
+        }
     }
 }
---
+++ src/main/kotlin/com/mituuz/fuzzier/components/SimpleFinderComponent.kt
@@ -0,0 +1,127 @@
+package com.mituuz.fuzzier.components
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.ui.EditorTextField
+import com.intellij.ui.components.JBList
+import com.intellij.ui.components.JBScrollPane
+import com.intellij.uiDesigner.core.GridConstraints
+import com.intellij.uiDesigner.core.GridLayoutManager
+import com.mituuz.fuzzier.StringEvaluator
+import com.intellij.openapi.components.service
+import com.mituuz.fuzzier.settings.FuzzierSettingsService
+import org.apache.commons.lang3.StringUtils
+import java.awt.Dimension
+import java.util.*
+import java.util.concurrent.Future
+import javax.swing.DefaultListModel
+import javax.swing.JPanel
+import javax.swing.SwingUtilities
+import kotlin.concurrent.schedule
+
+class SimpleFinderComponent(val project: Project) : JPanel(){
+    var fileList = JBList<String?>()
+    var searchField = EditorTextField()
+    private var debounceTimer: TimerTask? = null
+    @Volatile
+    var currentTask: Future<*>? = null
+    val fuzzierSettingsService = service<FuzzierSettingsService>()
+
+    init {
+        layout = GridLayoutManager(2, 1)
+        val scrollPane = JBScrollPane()
+        scrollPane.setViewportView(fileList)
+
+        add(
+            scrollPane,
+            GridConstraints(
+                0,
+                0,
+                1,
+                1,
+                GridConstraints.ANCHOR_CENTER,
+                GridConstraints.FILL_BOTH,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_WANT_GROW,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_WANT_GROW,
+                Dimension(-1, 300),
+                Dimension(-1, -1),
+                null,
+                0,
+                false
+            )
+        )
+        add(
+            searchField,
+            GridConstraints(
+                1,
+                0,
+                1,
+                1,
+                GridConstraints.ANCHOR_WEST,
+                GridConstraints.FILL_HORIZONTAL,
+                GridConstraints.SIZEPOLICY_WANT_GROW,
+                GridConstraints.SIZEPOLICY_FIXED,
+                null,
+                Dimension(-1, -1),
+                null,
+                0,
+                false
+            )
+        )
+
+        // Add a listener that updates the search list every time a change is made
+        val document = searchField.document
+        document.addDocumentListener(object : DocumentListener {
+            override fun documentChanged(event: DocumentEvent) {
+                debounceTimer?.cancel()
+                val debouncePeriod = fuzzierSettingsService.state.debouncePeriod
+                debounceTimer = Timer().schedule(debouncePeriod.toLong()) {
+                    updateListContents(project, searchField.text)
+                }
+            }
+        })
+    }
+
+    fun updateListContents(project: Project, searchString: String) {
+        if (StringUtils.isBlank(searchString)) {
+            SwingUtilities.invokeLater {
+                fileList.model = DefaultListModel()
+            }
+            return
+        }
+
+        val stringEvaluator = StringEvaluator(fuzzierSettingsService.state.multiMatch,
+            fuzzierSettingsService.state.exclusionList, fuzzierSettingsService.state.matchWeightSingleChar,
+            fuzzierSettingsService.state.matchWeightStreakModifier,
+            fuzzierSettingsService.state.matchWeightPartialPath)
+
+        currentTask?.takeIf { !it.isDone }?.cancel(true)
+
+        currentTask = ApplicationManager.getApplication().executeOnPooledThread {
+            fileList.setPaintBusy(true)
+            val listModel = DefaultListModel<StringEvaluator.FuzzyMatchContainer>()
+            val projectFileIndex = ProjectFileIndex.getInstance(project)
+            val projectBasePath = project.basePath
+
+            val contentIterator = projectBasePath?.let { stringEvaluator.getContentIterator(it, searchString, listModel) }
+
+            if (contentIterator != null) {
+                projectFileIndex.iterateContent(contentIterator)
+            }
+            val sortedList = listModel.elements().toList().sortedByDescending { it.score }
+            val valModel = DefaultListModel<String>()
+            sortedList.forEach { valModel.addElement(it.string) }
+
+            SwingUtilities.invokeLater {
+                fileList.model = valModel
+                fileList.setPaintBusy(false)
+                if (!fileList.isEmpty) {
+                    fileList.setSelectedValue(valModel[0], true)
+                }
+            }
+        }
+    }
+}
