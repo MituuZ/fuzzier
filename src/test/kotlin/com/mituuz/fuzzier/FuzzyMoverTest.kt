c877f3d96861cb9385a6018396c152261cc7497b
Mitja Leino <mituuuuz@hotmail.com>
2024-02-26 17:14:39 +0200

    Add initial test file

---
+++ src/test/kotlin/com/mituuz/fuzzier/FuzzyMoverTest.kt
@@ -0,0 +1,11 @@
+package com.mituuz.fuzzier
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class FuzzyMoverTest {
+    @Test
+    fun `Check that files are moved correctly`() {
+
+    }
+}
