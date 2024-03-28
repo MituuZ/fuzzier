b0591986f6e65cd4add935a5531ce4b1c0417973
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2024-03-28 18:36:58 +0100

    Revert "Update StringEvaluator.kt"
    This reverts commit 112273441d9b6646ca8b6a0c98fb5026a5ee2d84.

--- src/main/kotlin/com/mituuz/fuzzier/StringEvaluator.kt
+++ src/main/kotlin/com/mituuz/fuzzier/StringEvaluator.kt
@@ -12,14 +12,16 @@ class StringEvaluator(
 
     fun getContentIterator(projectBasePath: String, searchString: String, listModel: DefaultListModel<FuzzyMatchContainer>): ContentIterator {
         return ContentIterator { file: VirtualFile ->
-            val filePath = projectBasePath.let { it1 -> file.path.removePrefix(it1) }
-            if (isExcluded(file, filePath)) {
-                return@ContentIterator true
-            }
-            if (filePath.isNotBlank()) {
-                val fuzzyMatchContainer = fuzzyContainsCaseInsensitive(filePath, searchString)
-                if (fuzzyMatchContainer != null) {
-                    listModel.addElement(fuzzyMatchContainer)
+            if (!file.isDirectory) {
+                val filePath = projectBasePath.let { it1 -> file.path.removePrefix(it1) }
+                if (isExcluded(file, filePath)) {
+                    return@ContentIterator true
+                }
+                if (filePath.isNotBlank()) {
+                    val fuzzyMatchContainer = fuzzyContainsCaseInsensitive(filePath, searchString)
+                    if (fuzzyMatchContainer != null) {
+                        listModel.addElement(fuzzyMatchContainer)
+                    }
                 }
             }
             true
