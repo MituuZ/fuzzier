945b144c0f0f5c231b11bfff62e4e55adf2f2bd5
Mitja Leino <mituuuuz@hotmail.com>
2024-03-31 20:12:43 +0300

    Add basic structures for the calculation

--- src/main/kotlin/com/mituuz/fuzzier/entities/ScoreCalculator.kt
+++ src/main/kotlin/com/mituuz/fuzzier/entities/ScoreCalculator.kt
@@ -5,8 +5,11 @@ import com.mituuz.fuzzier.settings.FuzzierSettingsService
 
 class ScoreCalculator(private val searchString: String) {
     // TODO: We can parse/handle the search string here, just once per search
-    private var currentIndex: Int = 0
-    private var longestStreak: Int = 0
+    private val searchStringParts = searchString.split(" ")
+
+    private var searchStringIndex: Int = 0
+    private var searchStringLength: Int = 0
+    private var filePathIndex: Int = 0
 
     // Set up the settings
     private val settings = service<FuzzierSettingsService>().state
@@ -14,18 +17,67 @@ class ScoreCalculator(private val searchString: String) {
     private val matchWeightStreakModifier = settings.matchWeightStreakModifier
     private val matchWeightPartialPath = settings.matchWeightPartialPath
 
+    private var currentFilePath = ""
+    private var longestStreak: Int = 0
+
     /**
      * Returns null if no match can be found
      */
-    fun calculateScore(filePath: String, filename: String): Int? {
-        currentIndex = 0
+    fun calculateScore(filePath: String, filename: String): Int? { // TODO: This should return FuzzyScore
+        this.currentFilePath = filePath
         longestStreak = 0
 
         // Check if the search string is longer than the file path, which results in no match
-        if (searchString.length > filePath.length) { // TODO: + tolerance when it is implemented
+        if (searchString.length > currentFilePath.length) { // TODO: + tolerance when it is implemented
             return null
         }
 
+        for (part in searchStringParts) {
+            // Reset the index and length for the new part
+            searchStringIndex = 0
+            searchStringLength = part.length
+            filePathIndex = 0
+
+            if (!processString(part)) {
+                return null
+            }
+        }
+
         return 0
+    }
+
+    /**
+     * Returns false if no match can be found, this stops the search
+     */
+    private fun processString(searchStringPart: String): Boolean {
+        var searchStringIndex = 0;
+        while (searchStringIndex < searchStringLength) {
+            if (!canSearchStringBeContained()) {
+                return false
+            }
+
+            if (!processChar()) {
+                return false
+            }
+            searchStringIndex++
+        }
+
+        return true
+    }
+
+    private fun processChar(): Boolean {
+        return true
+    }
+
+    /**
+     * Checks if the remaining search string can be contained in the remaining file path
+     * e.g. if the remaining search string is "abc" and the remaining file path is "abcdef", it can be contained
+     * e.g. if the remaining search string is "abc" and the remaining file path is "def", it can't be contained
+     * e.g. if the remaining search string is "abc" and the remaining file path is "ab", it can't be contained
+     */
+    private fun canSearchStringBeContained(): Boolean {
+        val remainingSearchStringLength = searchStringLength - searchStringIndex
+        val remainingFilePathLength = currentFilePath.length - filePathIndex
+        return remainingSearchStringLength <= remainingFilePathLength // TODO: + tolerance when it is implemented
     }
 }
