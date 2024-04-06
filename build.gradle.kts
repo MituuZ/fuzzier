d7b556446087db714d885f3ef7502ca9ec24bc3f
Mitja Leino <mituuuuz@hotmail.com>
2024-04-06 06:59:17 +0300

    Update plugin and IntelliJ versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.9.22"
-  id("org.jetbrains.intellij") version "1.17.0"
+  id("org.jetbrains.kotlin.jvm") version "1.9.23"
+  id("org.jetbrains.intellij") version "1.17.3"
 }
 
 group = "com.mituuz"
@@ -24,7 +24,7 @@ tasks.test {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.3.4")
+  version.set("2024.1")
   plugins.set(listOf(/* Plugin Dependencies */))
 }
 
