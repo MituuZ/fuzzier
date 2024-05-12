ff9f639034f7a02b73e160c35a714b274e96b402
Mitja Leino <mituuuuz@hotmail.com>
2024-05-12 11:22:02 +0300

    Add placeholder actions for keymapping

---
+++ src/main/kotlin/com/mituuz/fuzzier/util/MoveUpAction.kt
@@ -0,0 +1,10 @@
+package com.mituuz.fuzzier.util
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class MoveUpAction : AnAction(){
+    override fun actionPerformed(e: AnActionEvent) {
+        // An empty implementation. This class is only used as a keymap reference
+    }
+}
