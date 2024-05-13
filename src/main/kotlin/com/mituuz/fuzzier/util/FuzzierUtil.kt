64a7f9926597262050f51cfced8cffd200f90f97
Mitja Leino <mituuuuz@hotmail.com>
2024-05-13 13:25:19 +0300

    Improve module root path logic

--- src/main/kotlin/com/mituuz/fuzzier/util/FuzzierUtil.kt
+++ src/main/kotlin/com/mituuz/fuzzier/util/FuzzierUtil.kt
@@ -1,6 +1,8 @@
 package com.mituuz.fuzzier.util
 
 import com.intellij.openapi.components.service
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.rootManager
 import com.mituuz.fuzzier.entities.FuzzyMatchContainer
 import com.mituuz.fuzzier.settings.FuzzierSettingsService
 import java.util.*
@@ -53,4 +55,40 @@ class FuzzierUtil {
     fun setPrioritizeShorterDirPaths(prioritizeShortedFilePaths: Boolean) {
         this.prioritizeShorterDirPaths = prioritizeShortedFilePaths;
     }
+
+    /**
+     * Calculate the number of unique root paths in the project by comparing module paths against each other
+     * Handles cases where some modules are nested under a single project root
+     *
+     * @return true if at least one module has a non-compatible root path, false if all paths can be merged
+     */
+    fun hasMultipleUniqueRootPaths(moduleManager: ModuleManager): Boolean {
+        var currentRoot: String? = null
+        for (module in moduleManager.modules) {
+            val contentRoots = module.rootManager.contentRoots
+            if (contentRoots.isEmpty()) {
+                continue
+            }
+            val moduleBasePath = contentRoots[0]?.path ?: continue
+
+            // Initial case, set the first root
+            if (currentRoot == null) {
+                currentRoot = moduleBasePath
+                continue
+            }
+
+            // Check if either root is contained in the other
+            if (currentRoot.contains(moduleBasePath) || moduleBasePath.contains(currentRoot)) {
+                // Update current root if new root is shorter
+                if (moduleBasePath.length < currentRoot.length) {
+                    currentRoot = moduleBasePath
+                }
+            } else {
+                // Root wasn't contained, multiple root paths are present
+                return true
+            }
+        }
+        return false
+    }
+
 }
