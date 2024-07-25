2f08388a2f67eb8bfa68db65d58ad957b32b1039
Mitja Leino <mitja.leino@hotmail.com>
2024-07-25 19:53:16 +0300

    Set up UAST

--- src/main/kotlin/com/mituuz/fuzzier/FuzzierFS.kt
+++ src/main/kotlin/com/mituuz/fuzzier/FuzzierFS.kt
@@ -2,23 +2,15 @@ package com.mituuz.fuzzier
 
 import com.intellij.ide.structureView.StructureViewModel
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder
-import com.intellij.ide.util.FileStructureFilter
-import com.intellij.ide.util.FileStructurePopup
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.ex.util.EditorUtil
-import com.intellij.openapi.fileChooser.impl.FileTreeStructure
 
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.findPsiFile
-import com.intellij.psi.PsiFile
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.testFramework.utils.vfs.getPsiFile
-import com.intellij.ui.speedSearch.ElementFilter
-import com.intellij.ui.tree.StructureTreeModel
-import com.intellij.ui.treeStructure.filtered.FilteringTreeStructure
+import com.intellij.psi.*
+import org.jetbrains.uast.toUElement
 
 class FuzzierFS : Fuzzier() {
     override var title: String = "Fuzzy Search (File Structure)"
@@ -31,9 +23,21 @@ class FuzzierFS : Fuzzier() {
         if (currentEditor != null) {
             ApplicationManager.getApplication().runReadAction() {
                 val psiFile: PsiFile? = currentEditor.file.findPsiFile(project)
+
                 val builder: TreeBasedStructureViewBuilder = currentEditor.structureViewBuilder as TreeBasedStructureViewBuilder
                 val treeModel: StructureViewModel = builder.createStructureViewModel(EditorUtil.getEditorEx(currentEditor));
+
+                // JavaRecursiveElementVisitor
+
+//                PsiElement
+//
+//                PsiClass.getMethods();
+//                val ps: PsiLocalVariable
+
                 if (psiFile != null) {
+                    val rec = Visitor()
+                    // psiFile.accept(rec)
+                    // PsiTreeUtil.collectElementsOfType()
 //                    val elements = PsiTreeUtil.getChildrenOfAnyType(psiFile, KtClass::class.java, KtNamedFunction::class.java, KtProperty::class.java)
 //                    for (element in elements) {
 //                        when (element) {
@@ -45,7 +49,8 @@ class FuzzierFS : Fuzzier() {
                     // Here's a KtNamedFunction when looking at FuzzyAction
                     val elements = psiFile.children[6].children[2].children // as KtNamedFunction
                     for (element in elements) {
-                        println(element)
+                        val asd = element.toUElement();
+                        println(asd)
                     }
                 }
                 psiFile?.processChildren { c ->
@@ -64,5 +69,16 @@ class FuzzierFS : Fuzzier() {
 
         // com.intellij.psi.SyntaxTraverser could be used for more powerful tree navigation
         super.updateListContents(project, searchString)
+    }
+
+    class Visitor : PsiRecursiveElementWalkingVisitor() {
+        override fun visitFile(file: PsiFile) {
+            super.visitFile(file)
+        }
+
+        override fun visitElement(element: PsiElement) {
+            println(element)
+            super.visitElement(element)
+        }
     }
 }
