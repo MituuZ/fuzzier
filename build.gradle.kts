461ef9c5b7a5e9d5c0997fbe725c515d063ec508
Mitja Leino <mitja.leino@hotmail.com>
2025-03-02 07:09:43 +0200

    Move jmh

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
   testRuntimeOnly(libs.junit4)
 
     // JMH dependencies
-  implementation("org.openjdk.jmh:jmh-core:1.37")
-  annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+  implementation(libs.jmhCore)
+  annotationProcessor(libs.jmhAnnprocessor)
   jmh("org.jetbrains.kotlin:kotlin-stdlib:2.1.10")
   jmh(fileTree("./libs") { include("*.jar") }) // libs folder contains idea:ideaIC:2024.3 jars
 }
