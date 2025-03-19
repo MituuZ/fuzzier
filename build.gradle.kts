4b5ecda2dcad72eaec6572813dd38955f242ce52
= <mitja@mituuz.com>
2025-03-19 21:15:26 +0200

    Correct change notes

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,24 +16,7 @@ intellijPlatform {
     changeNotes = """
     <h2>Version $currentVersion</h2>
     <ul>
-      <li>Update gradle and plugins</li>
-      <li>Improve FuzzyGrep PATH handling by using GeneralCommandline
-        <ul>
-          <li>Thanks to <a href="https://github.com/gymynnym">gymynnym</a> for finding the issue and the solution</li>
-        </ul>
-      </li>
-      <li>Indicate which command FuzzyGrep is running</li>
       <li>Add Fuzzy Grep (Case Insensitive) action</li>
-    </ul>
-    <h2>Version 1.6.0</h2>
-    <ul>
-      <li>Introduce first version of Fuzzy Grep
-        <ul>
-          <li>Call <a href="https://github.com/BurntSushi/ripgrep">ripgrep</a> in the background</li>
-          <li>Attempt to fall back to <code>grep</code> or <code>findstr</code> if <code>rg</code> is not found</li>
-          <li>The following action has been added: <code>map &lt;Leader&gt;ff &lt;action&gt;(com.mituuz.fuzzier.FuzzyGrep)</code></li>
-        </ul>
-      </li>
     </ul>
     """.trimIndent()
 
