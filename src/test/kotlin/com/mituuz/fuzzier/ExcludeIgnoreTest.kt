9f9845595c2bc8a5b133cacd458c7f3ee804abef
= <mitja@mituuz.com>
2025-06-07 10:16:46 +0300

    Revert separator replaces

--- src/test/kotlin/com/mituuz/fuzzier/ExcludeIgnoreTest.kt
+++ src/test/kotlin/com/mituuz/fuzzier/ExcludeIgnoreTest.kt
@@ -27,140 +27,92 @@ package com.mituuz.fuzzier
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.io.File
 
 class ExcludeIgnoreTest {
     private var testUtil = TestUtil()
-    private val separator = File.separator
 
     @Test
     fun excludeListTest() {
-        val filePaths = listOf(
-            "src" + separator + "main.kt",
-            "src" + separator + "asd" + separator + "main.kt",
-            "src" + separator + "asd" + separator + "asd.kt",
-            "src" + separator + "not" + separator + "asd.kt",
-            "src" + separator + "nope"
-        )
+        val filePaths = listOf("src/main.kt", "src/asd/main.kt", "src/asd/asd.kt", "src/not/asd.kt", "src/nope")
         val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf("asd", "nope"))
         Assertions.assertEquals(1, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "main.kt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/main.kt", filePathContainer.get(0).filePath)
     }
 
     @Test
     fun excludeListTestNoMatches() {
-        val filePaths = listOf(
-            "src" + separator + "main.kt",
-            "src" + separator + "not.kt",
-            "src" + separator + "dsa" + separator + "not.kt"
-        )
+        val filePaths = listOf("src/main.kt", "src/not.kt", "src/dsa/not.kt")
         val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf("asd"))
         Assertions.assertEquals(3, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "main.kt", filePathContainer.get(2).filePath)
-        Assertions.assertEquals("" + separator + "not.kt", filePathContainer.get(1).filePath)
-        Assertions.assertEquals("" + separator + "dsa" + separator + "not.kt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/main.kt", filePathContainer.get(2).filePath)
+        Assertions.assertEquals("/not.kt", filePathContainer.get(1).filePath)
+        Assertions.assertEquals("/dsa/not.kt", filePathContainer.get(0).filePath)
     }
-
 
     @Test
     fun excludeListTestEmptyList() {
-        val filePaths = listOf(
-            "src" + separator + "main.kt",
-            "src" + separator + "not.kt",
-            "src" + separator + "dsa" + separator + "not.kt"
-        )
+        val filePaths = listOf("src/main.kt", "src/not.kt", "src/dsa/not.kt")
         val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf())
         Assertions.assertEquals(3, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "main.kt", filePathContainer.get(2).filePath)
-        Assertions.assertEquals("" + separator + "not.kt", filePathContainer.get(1).filePath)
-        Assertions.assertEquals("" + separator + "dsa" + separator + "not.kt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/main.kt", filePathContainer.get(2).filePath)
+        Assertions.assertEquals("/not.kt", filePathContainer.get(1).filePath)
+        Assertions.assertEquals("/dsa/not.kt", filePathContainer.get(0).filePath)
     }
 
     @Test
     fun excludeListTestStartsWith() {
-        val filePaths = listOf(
-            "src" + separator + "main.kt",
-            "src" + separator + "asd" + separator + "main.kt",
-            "src" + separator + "asd" + separator + "asd.kt",
-            "src" + separator + "not" + separator + "asd.kt"
-        )
-        val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf("" + separator + "asd*"))
+        val filePaths = listOf("src/main.kt", "src/asd/main.kt", "src/asd/asd.kt", "src/not/asd.kt")
+        val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf("/asd*"))
         Assertions.assertEquals(2, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "not" + separator + "asd.kt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/not/asd.kt", filePathContainer.get(0).filePath)
     }
 
     @Test
     fun excludeListTestEndsWith() {
-        val filePaths = listOf(
-            "src" + separator + "main.log",
-            "src" + separator + "asd" + separator + "main.log",
-            "src" + separator + "asd" + separator + "asd.kt",
-            "src" + separator + "not" + separator + "asd.kt",
-            "src" + separator + "nope"
-        )
+        val filePaths = listOf("src/main.log", "src/asd/main.log", "src/asd/asd.kt", "src/not/asd.kt", "src/nope")
         val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf("*.log"))
         Assertions.assertEquals(3, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "asd" + separator + "asd.kt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/asd/asd.kt", filePathContainer.get(0).filePath)
     }
 
     @Test
     fun testIgnoreOneFile() {
-        val filePaths = listOf("src" + separator + "ignore-me.kt", "src" + separator + "main.kt")
-        val filePathContainer =
-            testUtil.setUpModuleFileIndex(filePaths, setOf(), listOf("src" + separator + "ignore-me.kt"))
+        val filePaths = listOf("src/ignore-me.kt", "src/main.kt")
+        val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf(), listOf("src/ignore-me.kt"))
         Assertions.assertEquals(1, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "main.kt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/main.kt", filePathContainer.get(0).filePath)
     }
 
     @Test
     fun testIgnoreEmptyList() {
-        val filePaths = listOf(
-            "src" + separator + "dir" + separator + "file.txt",
-            "src" + separator + "main.kt",
-            "src" + separator + "other.kt"
-        )
+        val filePaths = listOf("src/dir/file.txt", "src/main.kt", "src/other.kt")
         val filePathContainer = testUtil.setUpModuleFileIndex(filePaths, setOf(), listOf())
         Assertions.assertEquals(3, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "dir" + separator + "file.txt", filePathContainer.get(0).filePath)
-        Assertions.assertEquals("" + separator + "main.kt", filePathContainer.get(1).filePath)
-        Assertions.assertEquals("" + separator + "other.kt", filePathContainer.get(2).filePath)
+        Assertions.assertEquals("/dir/file.txt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/main.kt", filePathContainer.get(1).filePath)
+        Assertions.assertEquals("/other.kt", filePathContainer.get(2).filePath)
     }
 
     @Test
     fun testIgnoreMultipleFiles() {
-        val filePaths = listOf(
-            "src" + separator + "dir" + separator + "file.txt",
-            "src" + separator + "main.kt",
-            "src" + separator + "other.kt"
-        )
-        val filePathContainer = testUtil.setUpModuleFileIndex(
-            filePaths,
-            setOf(),
-            listOf("src" + separator + "dir" + separator + "file.txt", "src" + separator + "other.kt")
-        )
+        val filePaths = listOf("src/dir/file.txt", "src/main.kt", "src/other.kt")
+        val filePathContainer =
+            testUtil.setUpModuleFileIndex(filePaths, setOf(), listOf("src/dir/file.txt", "src/other.kt"))
         Assertions.assertEquals(1, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "main.kt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/main.kt", filePathContainer.get(0).filePath)
     }
 
     @Test
     fun testIgnoreInCombinationWithExclusionList() {
         /* for a FuzzierVCS action only the ignore list should be applied, and the exclusions should be skipped */
-        val filePaths = listOf(
-            "src" + separator + "dir" + separator + "file.txt",
-            "src" + separator + "main.kt",
-            "src" + separator + "other.kt",
-            "src" + separator + "ignore-me.kt",
-            "src" + separator + "exclude-me.kt"
-        )
-        val filePathContainer = testUtil.setUpModuleFileIndex(
-            filePaths,
-            setOf("dir", "exclude-me.kt"),
-            listOf("src" + separator + "ignore-me.kt")
-        )
+        val filePaths =
+            listOf("src/dir/file.txt", "src/main.kt", "src/other.kt", "src/ignore-me.kt", "src/exclude-me.kt")
+        val filePathContainer =
+            testUtil.setUpModuleFileIndex(filePaths, setOf("dir", "exclude-me.kt"), listOf("src/ignore-me.kt"))
         Assertions.assertEquals(4, filePathContainer.size())
-        Assertions.assertEquals("" + separator + "dir" + separator + "file.txt", filePathContainer.get(0).filePath)
-        Assertions.assertEquals("" + separator + "main.kt", filePathContainer.get(1).filePath)
-        Assertions.assertEquals("" + separator + "other.kt", filePathContainer.get(2).filePath)
-        Assertions.assertEquals("" + separator + "exclude-me.kt", filePathContainer.get(3).filePath)
+        Assertions.assertEquals("/dir/file.txt", filePathContainer.get(0).filePath)
+        Assertions.assertEquals("/main.kt", filePathContainer.get(1).filePath)
+        Assertions.assertEquals("/other.kt", filePathContainer.get(2).filePath)
+        Assertions.assertEquals("/exclude-me.kt", filePathContainer.get(3).filePath)
     }
 }
